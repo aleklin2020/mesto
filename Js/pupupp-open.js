86c1a141ae534dc3ba35947e30b01e0867992016
Alex <Alekseu.dok@yandex.ru>
2021-01-23 15:35:56 +0300

    fix: Ревью кода

--- Js/pupupp-open.js
+++ Js/pupupp-open.js
@@ -5,24 +5,21 @@ let formOpen = document.querySelector(".profile__bedit-button")
 let formClose = document.querySelector(".popup__close-icon")
 let nameProfile = document.querySelector(".profile__name")
 let jobProfile = document.querySelector(".profile__profession")
-
-		function open () { 
-			  	formElement.classList.add("popup_popup_opened")
-			  	nameInput.value = (nameProfile.textContent)
-			  	jobInput.value = (jobProfile.textContent)
-			  }
-		function close () {
-					formElement.classList.remove("popup_popup_opened")
-				}	
-				
+function open () { 
+	formElement.classList.add("popup_popup_opened")
+	nameInput.value = (nameProfile.textContent)
+	jobInput.value = (jobProfile.textContent)
+}
+function close () {
+	formElement.classList.remove("popup_popup_opened")
+}	
 
 function formSubmitHandler (evt) {
-   evt.preventDefault(); 
-   nameProfile.textContent = (nameInput.value)
-   jobProfile.textContent = (jobInput.value)
+	evt.preventDefault(); 
+	nameProfile.textContent = (nameInput.value)
+	jobProfile.textContent = (jobInput.value)
 	formElement.classList.remove("popup_popup_opened") 
 }
-
 formClose.addEventListener("click" , close);
 formOpen.addEventListener('click', open);
 formElement.addEventListener('submit', formSubmitHandler); 

ac095b0e7e39ef1f51d641b951da115c8542ff72
Alex <Alekseu.dok@yandex.ru>
2021-07-15 01:10:55 +0300

    FIX:Исправление карточек

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,20 +5,20 @@ export class UserInfo {
       this._userAvatar = userAvatar;
     }
 
-    //публичный метод - возвращает объект с данными пользователя
+    
     getUserInfo() {
       const userName = this._userName.textContent;
       const userProfession = this._userProfession.textContent;
       return {userName, userProfession};
     }
 
-    //публичный метод - принимает новые данные пользователя и добавляет их на страницу
+    
     setUserInfo(userName, userProfession) {
       if(userName) { this._userName.textContent = userName; }
       if(userProfession) { this._userProfession.textContent = userProfession; }
     }
 
-    //публичный метод - принимает новый аватар пользователя и добавляет их на страницу
+    
     setAvatar(userAvatar) {
       if(userAvatar) {this._userAvatar.src = userAvatar;}
       if(userAvatar) {this._userAvatar.alt = userAvatar;}
